f0c5aa954690d15ed7dc3af0e790c1e024a3204d
xbhog <2251195014@qq.com>
2023-01-31 14:26:39 +0800

    feat：全局唯一ID生成器

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,46 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author xbhog
+ * @describe:全局唯一ID
+ * @date 2023/1/30
+ */
+@Component
+public class RedisIdWorker {
+    //开始时间戳
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    //序列号的位数
+    private static final short COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    /**
+     * 使用long来接收，8字节-64比特位实现：分为符号位-时间戳-序列号
+     * @param keyPrefix
+     * @return
+     */
+    public long nextId(String keyPrefix){
+        //生成时间戳
+        LocalDateTime dateTime = LocalDateTime.now();
+        //秒数设置时区
+        long nowSecond = dateTime.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+        //生成序列号
+        //获取当日日期，精确到天
+        String date = dateTime.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        //左移32位(时间戳)，右32补零(序列号)，然后进行与运算获得64位
+        return timestamp << COUNT_BITS | count;
+    }
+}
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,15 +1,23 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @SpringBootTest
 class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+
 
     /**
      * 增加活动热key
@@ -18,5 +26,24 @@ class HmDianPingApplicationTests {
     @Test
     public void productTest() throws Exception {
         shopService.saveShop2Redis(1L,10L);
+    }
+    @Test
+    public  void testIdWorker() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(300);
+        ExecutorService executorService = Executors.newFixedThreadPool(300);
+        Runnable task = ()->{
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id"+id);
+            }
+            countDownLatch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            executorService.submit(task);
+        }
+        countDownLatch.await();
+        long endTime = System.currentTimeMillis();
+        System.out.println("time= "+(endTime-begin));
     }
 }
