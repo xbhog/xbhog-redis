b584c3f744829e6407c69de956531401957c32c7
xbhog <2251195014@qq.com>
2023-02-01 10:42:17 +0800

    feat：增加优惠卷下单功能

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,10 +2,14 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
+import com.hmdp.mapper.SeckillVoucherMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -18,8 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return Result.fail("功能未完成");
+        return seckillVoucherService.seckillVoucher(voucherId);
     }
 }
--- src/main/java/com/hmdp/service/ISeckillVoucherService.java
+++ src/main/java/com/hmdp/service/ISeckillVoucherService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.SeckillVoucher;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ISeckillVoucherService extends IService<SeckillVoucher> {
 
+    Result seckillVoucher(Long voucherId);
 }
