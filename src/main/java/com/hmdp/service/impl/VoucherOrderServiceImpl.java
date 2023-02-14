49802e478b6289c30acb0fcd2d078fe8941e4f0b
xbhog <2251195014@qq.com>
2023-02-14 10:23:45 +0800

    feat:增加redisson分布式锁实现

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -11,6 +11,8 @@ import com.hmdp.service.IVoucherOrderService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +22,7 @@ import java.time.LocalDateTime;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -37,45 +39,54 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private IVoucherOrderService voucherOrderService;
     @Resource
     private RedisIdWorker redisIdWorker;
-
+    @Resource
+    private RedissonClient redissonClient;
 
     @Override
     public Result seckillVoucher(Long voucherId) {
         //查询优惠卷库存信息
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
-        log.info("查询秒杀优惠卷：{}",voucher);
+        log.info("查询秒杀优惠卷：{}", voucher);
         //判断秒杀是否开始：开始时间，结束时间
-        if(voucher.getBeginTime().isAfter(LocalDateTime.now())){
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
             return Result.fail("活动暂未开始，敬请期待！");
         }
-        if(voucher.getEndTime().isBefore(LocalDateTime.now())){
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
             return Result.fail("活动已结束，请关注下次活动！");
         }
         //判断库存是否充足
-        if(voucher.getStock() < 1){
+        if (voucher.getStock() < 1) {
             return Result.fail("库存不足，正在补充!");
         }
         Long userId = UserHolder.getUser().getId();
-
-        synchronized (userId.toString().intern()){
+        //加入分布式锁
+        RLock lock = redissonClient.getLock("lock:order" + userId);
+        if (!lock.tryLock()) {
+            return Result.fail("加锁失败");
+        }
+        try {
             //获取原始事务代理对象
             IVoucherOrderService iVoucherOrderService = (IVoucherOrderService) AopContext.currentProxy();
             return iVoucherOrderService.createVoucherOrder(voucherId);
+        } finally {
+            lock.unlock();
         }
+
     }
+
     @Override
     @Transactional
     public Result createVoucherOrder(Long voucherId) {
         Long userId = UserHolder.getUser().getId();
-        log.info("开始进行用户秒杀活动：{}",userId);
+        log.info("开始进行用户秒杀活动：{}", userId);
         //一人一单逻辑
         Integer count = voucherOrderService.query().eq("voucher_id", voucherId).eq("user_id", userId).count();
-        if(count > 0){
+        if (count > 0) {
             return Result.fail("该用户已参加活动。");
         }
         //开始扣减库存(通过乐观锁--->对应数据库中行锁实现)
-        boolean success  = seckillVoucherMapper.updateDateByVoucherId(voucherId);
-        if(!success){
+        boolean success = seckillVoucherMapper.updateDateByVoucherId(voucherId);
+        if (!success) {
             return Result.fail("库存不足，正在补充!");
         }
         //创建订单
